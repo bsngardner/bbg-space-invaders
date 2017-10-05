97ea7229120675e90a19c4a4add8563616337fca
Jordan Whitehead <jordan9001@gmail.com>
2017-10-04 23:33:44 -0600

    First!

---
+++ src/vdma.h
@@ -0,0 +1,14 @@
+/*
+ * vdma.h
+ *
+ *  Created on: Oct 4, 2017
+ *      Author: superman
+ */
+
+#ifndef VDMA_H_
+#define VDMA_H_
+#include "xil_types.h"
+
+void vdma_init(u32 * frame0, u32 * frame1);
+
+#endif /* VDMA_H_ */
