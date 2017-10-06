4746b1941cabe9e5f755447e489aa484d7dc7f51
bsngardner <bsn.gardner@gmail.com>
2017-10-05 21:30:05 -0600

    added lookup table file.

---
+++ table.c
@@ -0,0 +1,29 @@
+/*
+ * table.c
+ *
+ *  Created on: Oct 5, 2017
+ *      Author: superman
+ */
+
+#include "xil_types.h"
+
+#define BIT(x) (1 << (x))
+#define BIT0 BIT(0)
+#define BIT1 BIT(1)
+#define BIT2 BIT(2)
+#define BIT3 BIT(3)
+#define BIT4 BIT(4)
+#define BIT5 BIT(5)
+#define BIT6 BIT(6)
+#define BIT7 BIT(7)
+#define BIT8 BIT(8)
+#define BIT9 BIT(9)
+#define BIT10 BIT(10)
+#define BIT11 BIT(11)
+#define BIT12 BIT(12)
+#define BIT13 BIT(13)
+#define BIT14 BIT(14)
+#define BIT15 BIT(15)
+
+const u16 table_bit[] = { BIT0, BIT1, BIT2, BIT3, BIT4, BIT5, BIT6, BIT7, BIT8,
+		BIT9, BIT10, BIT11, BIT12, BIT13, BIT14, BIT15 };
---
+++ table.h
@@ -0,0 +1,13 @@
+/*
+ * table.h
+ *
+ *  Created on: Oct 5, 2017
+ *      Author: superman
+ */
+
+#ifndef TABLE_H_
+#define TABLE_H_
+
+extern const u16 table_bit[];
+
+#endif /* TABLE_H_ */
