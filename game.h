561ad081da73dded3dc3a2dadd1cfcf9656f736f
Ben <bfgard@gmail.com>
2017-10-05 23:46:22 -0600

    added the enum

--- game.h
+++ game.h
@@ -1,18 +1,33 @@
 /*
  * game.h
  *
+ * Contains typedefs and defines for the game
+ *
  *  Created on: Oct 4, 2017
- *      Author: superman
+ *      Author:  Broderick Gardner
+ *      		Benjamin Gardner
  */
 
 #ifndef GAME_H_
 #define GAME_H_
 
+#define ALIEN_ROWS 5 //number of alien rows
+
+#define GAME_H (SCREEN_H/2) //game height
+#define GAME_W (SCREEN_W/2) //game width
+
+#define GAME_BUNKER_COUNT 4 //number of bunkers
+#define GAME_BUNKER_POS (GAME_W/8-BMP_BUNKER_W/2) //location of bunkers
+#define GAME_BUNKER_SEP (GAME_W/4) //separation of the bunkers
+#define GAME_BUNKER_Y ((GAME_H*3)/4-BMP_BUNKER_H/2) //y position of the bunkers
+
+//Struct for coordinate point
 typedef struct {
 	s16 x;
 	s16 y;
 } point_t;
 
+//Tank struct, contains missile
 typedef struct {
 	point_t pos;
 	struct {
@@ -21,6 +36,7 @@ typedef struct {
 	} missile;
 } tank_t;
 
+//Alien missile type
 typedef struct {
 	point_t pos;
 	u16 active :1;
@@ -32,11 +48,12 @@ typedef struct {
 	} type;
 } alien_missiles_t;
 
+//Alien block
 typedef struct alien_block {
 	point_t pos;
 	s8 loffset;
 	s8 roffset;
-	u16 alien_status[5];
+	u16 alien_status[ALIEN_ROWS];
 	enum {
 		OUT = 0, IN = 1
 	} legs;
